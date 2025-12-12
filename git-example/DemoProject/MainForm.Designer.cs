4a025c9f663b2e6052d5b6309ee6dec82cc9c590
carina-sakura <anubis.cari@gmail.com>
2025-12-12 14:08:09 +0100

    Button Farbe geändert und Titel des Dings geändert

--- git-example/DemoProject/MainForm.Designer.cs
+++ git-example/DemoProject/MainForm.Designer.cs
@@ -35,29 +35,14 @@ namespace DemoProject
             // 
             // btnClickThis
             // 
-            //this.btnClickThis.Location = new System.Drawing.Point(12, 60);
-            //this.btnClickThis.Name = "btnClickThis";
-            //this.btnClickThis.Size = new System.Drawing.Size(81, 25);
-            //this.btnClickThis.TabIndex = 0;
-            //this.btnClickThis.Text = "Click this";
-            //this.btnClickThis.UseVisualStyleBackColor = true;
-            //this.btnClickThis.Click += new System.EventHandler(this.btnClickThis_Click);
-
-
-        //button green
+            this.btnClickThis.BackColor = System.Drawing.Color.Green;
             this.btnClickThis.Location = new System.Drawing.Point(12, 60);
             this.btnClickThis.Name = "btnClickThis";
             this.btnClickThis.Size = new System.Drawing.Size(81, 25);
             this.btnClickThis.TabIndex = 0;
             this.btnClickThis.Text = "Click this";
-            this.btnClickThis.UseVisualStyleBackColor = false;   // Wichtig: sonst ignoriert er die Farbe!
-            this.btnClickThis.BackColor = System.Drawing.Color.Green;  // 👉 Button wird grün
+            this.btnClickThis.UseVisualStyleBackColor = false;
             this.btnClickThis.Click += new System.EventHandler(this.btnClickThis_Click);
-           
-            
-            
-            
-            
             // 
             // lblHelloWorld
             // 
@@ -67,15 +52,16 @@ namespace DemoProject
             this.lblHelloWorld.Size = new System.Drawing.Size(0, 13);
             this.lblHelloWorld.TabIndex = 1;
             // 
-            // Form1
+            // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(325, 145);
             this.Controls.Add(this.lblHelloWorld);
             this.Controls.Add(this.btnClickThis);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.Name = "MainForm";
+            this.Text = "Demo Project";
+            this.Load += new System.EventHandler(this.MainForm_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
